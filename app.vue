5bd30859d0d536e0a7810dea74e954fe165e615c
hzy <3500412648@qq.com>
2023-10-15 15:16:40 +0800

    整合 naive ui 框架

--- app.vue
+++ app.vue
@@ -1,5 +1,30 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!--<NuxtWelcome />-->
+    <n-space>
+      <n-button>Default</n-button>
+      <n-button type="tertiary">
+        Tertiary
+      </n-button>
+      <n-button type="primary">
+        Primary
+      </n-button>
+      <n-button type="info">
+        Info
+      </n-button>
+      <n-button type="success">
+        Success
+      </n-button>
+      <n-button type="warning">
+        Warning
+      </n-button>
+      <n-button type="error">
+        Error
+      </n-button>
+    </n-space>
   </div>
 </template>
+
+<script setup>
+
+import {NButton} from "naive-ui"</script>
